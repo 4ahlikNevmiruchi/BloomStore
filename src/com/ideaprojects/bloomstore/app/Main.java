f5c0b5b6d2631fbec08588f43593924b0603d61a
mond <viktor.yudakov.1104@gmail.com>
2025-09-24 09:09:20 +0300

    Rose class created

--- src/com/ideaprojects/bloomstore/app/Main.java
+++ src/com/ideaprojects/bloomstore/app/Main.java
@@ -1,4 +1,12 @@
 package com.ideaprojects.bloomstore.app;
 
+import com.ideaprojects.bloomstore.model.Rose;
+
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Rose r = new Rose(5.0, 40, LocalDate.now().minusDays(2), 80, 12);
+        System.out.println(r.description());
+    }
 }
---
+++ src/com/ideaprojects/bloomstore/model/Rose.java
@@ -0,0 +1,25 @@
+package com.ideaprojects.bloomstore.model;
+
+import java.time.LocalDate;
+
+public class Rose extends Flower {
+    private int thornCount;
+
+    public Rose(double price, double stemLengthCm, LocalDate cutDate, int freshnessLevel, int thornCount) {
+        super("Rose", price, stemLengthCm, cutDate, freshnessLevel);
+        this.thornCount = Math.max(0,thornCount);
+    }
+
+    public int getThornCount() {
+        return thornCount;
+    }
+
+    public void setThornCount(int thornCount) {
+        this.thornCount = thornCount;
+    }
+
+    @Override
+    public String description() {
+        return String.format("%s - thorns: %d", super.description(), thornCount);
+    }
+}
